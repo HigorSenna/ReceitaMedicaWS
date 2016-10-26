d17b8372c3d362133f89d7a1fc5c533aa04b39b8
Higor Senna <higorr_jf@hotmail.com>
2016-10-26 00:06:33 -0200

    Inicializado criacao do dao,do servico rest e adicionado dependencia httpservlet

---
+++ ReceitaMedica/ReceitaMedica-ejb/src/main/java/DAO/MedicoDAO.java
@@ -0,0 +1,10 @@
+package DAO;
+
+import br.com.commons.ejb.dao.GenericoDAO;
+import model.Medico;
+
+public class MedicoDAO extends GenericoDAO<Medico, Integer>{
+
+	private static final long serialVersionUID = 1L;
+
+}
