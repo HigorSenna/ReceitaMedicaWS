1d0af8d17eb85fb41f8a588ee6595606d6887479
Higor Senna <higorr_jf@hotmail.com>
2016-11-02 14:23:24 -0200

    Criado ação de validar médico existente por CRM

--- ReceitaMedica/ReceitaMedica-web/src/main/java/controller/CadastroMedicoController.java
+++ ReceitaMedica/ReceitaMedica-web/src/main/java/controller/CadastroMedicoController.java
@@ -10,6 +10,7 @@ import javax.ws.rs.client.ClientBuilder;
 
 import enums.TipoMensagemEnum;
 import model.Medico;
+import service.MedicoService;
 import utils.MessagesUtils;
 import utils.ParamUtils;
 import utils.UrlUtils;
@@ -25,20 +26,33 @@ public class CadastroMedicoController implements Serializable{
 	@Inject
 	private Medico medico;
 	
+	@Inject 
+	MedicoService medicoService;
+	
 	public void cadastrarMedico(){
-		Client cliente = ClientBuilder.newClient();
-		try {			
-			cliente.target(UrlUtils.getURL(CADASTRO_MEDICO))
-			.queryParam(ParamUtils.NOME_MEDICO, medico.getNmMedico())
-			.queryParam(ParamUtils.CRM, medico.getCrmMedico())
-			.request()
-			.get();
-			
-			MessagesUtils.exibirMensagemRedirect("Medico cadastrado com sucesso", "cadastro.xhtml", TipoMensagemEnum.SUCESSO);
-			
-		} catch (Exception e) {
-			MessagesUtils.exibirMensagemRedirect("Falha ao cadastrar médico", "cadastro.xhtml", TipoMensagemEnum.ERRO);
+		if(!isMedicoExistente()){
+			Client cliente = ClientBuilder.newClient();
+			try {			
+				cliente.target(UrlUtils.getURL(CADASTRO_MEDICO))
+				.queryParam(ParamUtils.NOME_MEDICO, medico.getNmMedico())
+				.queryParam(ParamUtils.CRM, medico.getCrmMedico())
+				.request()
+				.get();
+				
+				MessagesUtils.exibirMensagemRedirect("Medico cadastrado com sucesso", "cadastro.xhtml", TipoMensagemEnum.SUCESSO);
+				
+			} catch (Exception e) {
+				MessagesUtils.exibirMensagemRedirect("Falha ao cadastrar médico", "cadastro.xhtml", TipoMensagemEnum.ERRO);
+			}
 		}
+		else{
+			MessagesUtils.exibirMensagemRedirect("Médico ja existe", "cadastro.xhtml", TipoMensagemEnum.ERRO);
+		}		
+	}
+	
+	private boolean isMedicoExistente(){
+		Medico med = medicoService.buscarPorCrm(medico.getCrmMedico());
+		return med != null;
 	}
 
 	public Medico getMedico() {
