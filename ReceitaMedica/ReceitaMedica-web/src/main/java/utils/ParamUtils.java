618ed8c7d7e2dee438f782589f0a14fec2dc85e8
Higor Senna <higorr_jf@hotmail.com>
2016-11-02 17:14:46 -0200

    modificado nome dos parametros

--- ReceitaMedica/ReceitaMedica-web/src/main/java/utils/ParamUtils.java
+++ ReceitaMedica/ReceitaMedica-web/src/main/java/utils/ParamUtils.java
@@ -5,11 +5,11 @@ public abstract class ParamUtils {
 	public static final String DOMINIO = "http://localhost:10080/ReceitaMedica-web/";
 	
 	/*Medico*/
-	public static final String CRM = "crm";
-	public static final String NOME_MEDICO = "nomeMedico";
+	public static final String CRM = "crmMedico";
+	public static final String NOME_MEDICO = "nmMedico";
 	
 	/*Paciente*/
-	public static final String CPF = "cpf";
-	public static final String NOME_PACIENTE = "nomePaciente";
+	public static final String CPF = "cpfPaciente";
+	public static final String NOME_PACIENTE = "nmPaciente";
 	
 }
