ed5509cff5ead6c1442452d7ba93db96a2883e8c
Higor Senna <higorr_jf@hotmail.com>
2016-11-02 18:00:29 -0200

    inicializado metodos para cadastrar receita.(sem itens)

--- ReceitaMedica/ReceitaMedica-ejb/src/main/java/service/ReceitaService.java
+++ ReceitaMedica/ReceitaMedica-ejb/src/main/java/service/ReceitaService.java
@@ -3,9 +3,23 @@ package service;
 import java.io.Serializable;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.inject.Inject;
+
+import DAO.ReceitaDAO;
+import model.ReceitasMedica;
 
 @Stateless
 public class ReceitaService implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private ReceitaDAO receitaDAO;
+	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public void salvar(ReceitasMedica receita) throws Exception{
+		receitaDAO.salvar(receita);
+	}
 }
--- ReceitaMedica/ReceitaMedica-web/src/main/java/controller/CadastroReceitaController.java
+++ ReceitaMedica/ReceitaMedica-web/src/main/java/controller/CadastroReceitaController.java
@@ -3,12 +3,26 @@ package controller;
 import java.io.Serializable;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import model.ReceitasMedica;
 
 @ViewScoped
 @Named
 public class CadastroReceitaController implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private ReceitasMedica receitaMedica;
+	
+	public ReceitasMedica getReceitaMedica() {
+		return receitaMedica;
+	}
+
+	public void setReceitaMedica(ReceitasMedica receitaMedica) {
+		this.receitaMedica = receitaMedica;
+	}
 
 }
