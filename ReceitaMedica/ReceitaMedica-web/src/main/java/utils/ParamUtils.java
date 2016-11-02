30e8eff0ca364028377605d3bdde07e32489716c
Higor Senna <higorr_jf@hotmail.com>
2016-11-02 17:59:41 -0200

    Adicionado parametros de receita medica

--- ReceitaMedica/ReceitaMedica-web/src/main/java/utils/ParamUtils.java
+++ ReceitaMedica/ReceitaMedica-web/src/main/java/utils/ParamUtils.java
@@ -12,4 +12,10 @@ public abstract class ParamUtils {
 	public static final String CPF = "cpfPaciente";
 	public static final String NOME_PACIENTE = "nmPaciente";
 	
+	/*receita*/
+	
+	public static final String FL_STATUS = "flStatus";
+	public static final String NUM_RECEITA = "numReceita";
+	public static final String DATA_RECEITA = "data";
+	
 }
