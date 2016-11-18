1938589ad44252058b3edb88fa5b133071008420
Higor Senna <higorr_jf@hotmail.com>
2016-11-18 17:03:52 -0200

    Ajustes no cadastro de medico

--- ReceitaMedica/ReceitaMedica-web/src/main/java/controller/CadastroMedicoController.java
+++ ReceitaMedica/ReceitaMedica-web/src/main/java/controller/CadastroMedicoController.java
@@ -7,15 +7,17 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.core.MediaType;
 
+import org.primefaces.json.JSONObject;
+
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 import enums.TipoMensagemEnum;
 import model.Medico;
-import service.MedicoService;
 import utils.JsonUtils;
 import utils.MessagesUtils;
+import utils.ParamUtils;
 import utils.UrlUtils;
 
 @ViewScoped
@@ -29,9 +31,6 @@ public class CadastroMedicoController implements Serializable{
 	@Inject
 	private Medico medico;
 	
-	@Inject 
-	MedicoService medicoService;
-	
 	private Client client;
 	private WebResource webResource;
 	private ClientResponse response;
@@ -39,11 +38,8 @@ public class CadastroMedicoController implements Serializable{
 	public void cadastrarMedico() {
 
 		client = Client.create();
-		Medico m = new Medico();
-		m.setCrmMedico("6118181");
-		m.setNmMedico("FULL TRAB");
 		try {
-			String json = JsonUtils.parseJson(m);
+			String json = JsonUtils.parseJson(medico);
 
 			webResource = client.resource(UrlUtils.getURL(CADASTRO_MEDICO));
 			response = webResource.type(MediaType.APPLICATION_JSON)
@@ -51,6 +47,10 @@ public class CadastroMedicoController implements Serializable{
 
 			json = response.getEntity(String.class);
 			System.out.println(json);
+			
+			JSONObject obj = JsonUtils.parseObject(json);
+			
+			MessagesUtils.exibirMensagemRedirect(obj.getString(ParamUtils.MESSAGE), "cadastro.xhtml", TipoMensagemEnum.SUCESSO);
 
 		} catch (Exception e) {
 			MessagesUtils.exibirMensagemRedirect("Falha ao cadastrar médico", "cadastro.xhtml", TipoMensagemEnum.ERRO);
